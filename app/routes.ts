ab2ff964089f6b1b8fd739a8238ccf9bb77db0a3
Michael Kanios <mkanios77@gmail.com>
2025-08-29 09:45:55 +0000

    ROutes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
+import { Route } from "react-router";
 
-export default [index("routes/home/home.tsx")] satisfies RouteConfig;
+export default [index("routes/home/home.tsx"),
+                
+
+
+] satisfies RouteConfig;
